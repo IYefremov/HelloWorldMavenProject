3b44f5c0710127307e78482267756ac0c5663e07
IYefremov <iyefremov@cyberiansoft.com>
2019-05-30 12:56:24 +0300

    New Branch

--- src/main/java/com/myPackage/Application.java
+++ src/main/java/com/myPackage/Application.java
@@ -5,6 +5,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("It's my first Maven project");
 
-        System.out.println("And this line will be in the new branch");
+        System.out.println("And this line will be in the new  branch");
     }
 }
